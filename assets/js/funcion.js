8ee44686204039d57c4a995b3089d79c7dfeec64
carlos alberto bustos <proyectosangular09@gmail.com>
2024-07-12 16:57:00 -0500

    Updates

---
+++ assets/js/funcion.js
@@ -0,0 +1,32 @@
+/*-jQuery(document).ready(function($) {
+        
+    if( localStorage.getItem('departamento') ){
+
+        let ValDepart = localStorage.getItem('departamento')
+        $(`#departID option[value='${ValDepart}']`).attr("selected", true);
+        
+    }else{
+    
+        $("#departID option[value='-1']").attr("selected", true);
+    }
+
+
+
+    if( localStorage.getItem('ciudad') ){
+
+        let ValCiudad = localStorage.getItem('ciudad')
+        $(`#ciudadID option[value='${ValCiudad}']`).attr("selected", true);
+    
+    }else{
+    
+        $("#ciudadID option[value='todos']").attr("selected", true);
+       
+    }
+
+
+})
+
+--*/
+
+
+
